eb8a21438f7c18885c7748a0f55fac25763dbdc1
thunder'thund.e.r <sudothunder@gmail.com>
2023-06-06 17:39:13 +0300

    kotab legetyo

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(i+1, 10):
-        if i != j and not (i == 0 and j == 1):
-            print("{:02d}, {:02d}".format(i, j), end=", ")
-print("{:02d}".format(9))
+for i in range(0, 10):
+    for j in range((i+1), 10):
+        if (i is not 8) or (j is not 9):
+            print("{}{}, ".format(i, j), end="")
+        else:
+            print("{}{}".format(i, j))
