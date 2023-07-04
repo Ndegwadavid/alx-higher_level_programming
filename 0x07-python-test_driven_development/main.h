4112d3fff247b0c5276d958c5d04544ef2761f32
sys <theabsolutejesuit@gmail.com>
2023-07-04 08:39:36 +0300

    all prototypes

--- 0x07-python-test_driven_development/main.h
+++ 0x07-python-test_driven_development/main.h
@@ -1,1 +1,6 @@
 def add_integer(a, b=98):
+def matrix_divided(matrix, div):
+def say_my_name(first_name, last_name=""):
+def print_square(size):
+def text_indentation(text):
+def max_integer(list=[]):
