224cec40c5745bf8767e97b7a655719e95dec471
david Njoroge <davidndegwa6869@gmail.com>
2023-09-15 10:22:46 +0300

    corrections" corrections

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,14 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
-if (args.length === 0) {
-  console.log('No argument');
-} else if (args.length === 1) {
-  console.log('Argument found');
-} else {
-  console.log('Arguments found');
+
+switch (args.length) {
+  case 0:
+    console.log('No argument');
+    break;
+  case 1:
+    console.log('Argument found');
+    break;
+  default:
+    console.log('Arguments found');
+    break;
 }
