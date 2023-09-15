19ecb7a9bc4015c3d64e9a1336fe4406bc2dd543
david Njoroge <davidndegwa6869@gmail.com>
2023-09-15 10:57:24 +0300

    final corrections

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-function executeFunctionXTimes(x, theFunction) {
-    if (x > 0) {
-      theFunction();
-      executeFunctionXTimes(x - 1, theFunction);
-    }
+function executeFunctionXTimes (x, theFunction) {
+  if (x > 0) {
+    theFunction();
+    executeFunctionXTimes(x - 1, theFunction);
   }
-  
+}
