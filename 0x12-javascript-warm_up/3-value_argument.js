7333f1bddf94426a601165bf82144a9c10efc6ad
david Njoroge <davidndegwa6869@gmail.com>
2023-11-13 10:58:41 +0300

    arg parser

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
+const arg = process.argv[2];
 
-if (args[0]) {
-  console.log(args[0]);
+if (arg === undefined) {
+  console.log('No argument');
 } else {
-  console.log("No argument");
+  console.log(arg);
 }
