ab7dcf5c55f859f05fd041fa4282ba6d715d95c1
david Njoroge <davidndegwa6869@gmail.com>
2024-01-03 20:39:07 +0300

    javascript web scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
 // script that reads and prints the content of a file
-
-
 const fs = require('fs');
 const filepath = process.argv[2];
 fs.readFile(filepath, 'utf-8', function (err, data) {
