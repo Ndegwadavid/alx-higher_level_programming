3dafcccd4af00aa6dff0cb0109c631be69ca9135
david Njoroge <davidndegwa6869@gmail.com>
2024-01-03 21:38:54 +0300

    write me js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,12 +1,7 @@
 #!/usr/bin/node
-// script that writes a string to a file
-
 const fs = require('fs');
-const args = process.argv;
-const text = args[2];
-const file = args[3];
-fs.writeFile(file, text, 'utf-8', (err) => {
-  if (err) {
-    console.log(err);
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) {
+    console.log(error);
   }
 });
