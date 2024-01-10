eadadcd6cd7254cb8277334e8a3f9cf42c3021d0
david Njoroge <davidndegwa6869@gmail.com>
2024-01-10 15:07:43 +0300

    javascript and query selectors

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,6 @@
+//javascript that changes and updates the text color of the <header> element to #FF0000
+document.addEventListener('DOMContentLoaded', function () {
+    const header = document.querySelector('header');
+    header.style.color = '#FF0000';
+  });
+  
